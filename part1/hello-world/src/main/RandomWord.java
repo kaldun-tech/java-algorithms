d0f59bdbc6d6f980dfefca2c2cf45fcdb4d064cc
Taras <tsmereka@protonmail.com>
2024-09-29 14:00:49 -0500

    Started RandomWord

--- part1/hello-world/src/main/RandomWord.java
+++ part1/hello-world/src/main/RandomWord.java
@@ -20,3 +20,22 @@
  *  java-algs4 RandomWord < animals8.txt
  *  emu
  */
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        String champion = "";
+        for (int i = 0; !StdIn.isEmpty(); ++i) {
+            double probability = 1 / i;
+            String nextWord = StdIn.readString();
+            if (StdRandom.bernoulli(probability)) {
+                champion = nextWord;
+            }
+        }
+        // Prints the champion
+        StdOut.println(champion);
+    }
+}
