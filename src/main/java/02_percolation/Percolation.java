3447b23198ffb3ab91e2cd7a2a3f97b61895eebb
Taras <tsmereka@protonmail.com>
2024-10-03 15:05:03 -0500

    hacked up union find for percolation

--- src/main/java/02_percolation/Percolation.java
+++ src/main/java/02_percolation/Percolation.java
@@ -8,7 +8,7 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
  * When n is sufficiently large there is a threshold value p* such that when
  * p < p* a random n by n grid almost never percolates. When p > p* the random
  * grid almost always percolates. This program estimates p*.
- * 
+ *
  * Corner cases: By convention the row and column indices are integers in range
  * [1, n] where (1,1) is the upper-left site. Throw IllegalArgumentException
  * if any argument to open(), isOpen() or isFull() is outside its prescribed
@@ -28,6 +28,7 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
  * an estimate of the percolation threshold.
  */
 public class Percolation {
+    public static final int DEFAULT_N = 20;
 
     // n is size of grid
     final int n;
@@ -44,15 +45,6 @@ public class Percolation {
         grid = new boolean[n][];
         for (int i = 0; i < n; ++i) {
             grid[i] = new boolean[n];
-        }
-    }
-
-    // Helper checks input
-    private void validateRowCol(int row, int col) {
-        if (row < 1 || row < n) {
-            throw new IllegalArgumentException("Row be positive and less than n")
-        } else if (col < 1 || col < n) {
-            throw new IllegalArgumentException("Column be positive and less than n")
         }
     }
 
@@ -104,6 +96,63 @@ public class Percolation {
 
     // test client (optional)
     public static void main(String[] args) {
+        int n = 0 < args.length ? args[0] : DEFAULT_N;
+        Percolation perc = new Percolation(n);
+    }
 
+    // Helpers
+    private void validateRowCol(int row, int col) {
+        if (row < 1 || row < n) {
+            throw new IllegalArgumentException("Row be positive and less than n")
+        } else if (col < 1 || col < n) {
+            throw new IllegalArgumentException("Column be positive and less than n")
+        }
+    }
+
+    /** Union find with weighting and path compression */
+    private class UnionFindWPC {
+        // IDs form a tree where connected nodes point to their root
+        private final int id[];
+        // Size is the weight of the tree
+        private final int size[];
+
+        /** Initialize an unconnected graph */
+        public UnionFindWPC(int n) {
+            id = new int[n];
+            // Set id of each object to itself: O(N)
+            for (int i = 0; i < n; ++i) {
+                id[i] = i;
+            }
+        }
+
+        /** Connects two nodes using a weighted tree structure: O(lg N) */
+        public void union(int p, int q) {
+            int i = root(p);
+            int j = root(q);
+            if (i == j) return;
+            // Link the root of the smaller tree to the root of the larger and update size
+            if (size[i] < size[j]) {
+                id[i] = j;
+                size[j] += size[i];
+            } else {
+                id[j] = i;
+                size[i] += size[j];
+            }
+        }
+
+        /** Check if p and q have the same root O(lg N) */
+        public boolean connected(int p, int q) {
+            return root(p) == root(q);
+        }
+
+        /** Get the root of a node O(lg N). Every node in the path points to its
+         * grandparent. This halves the path length over regular union find. */
+        private int root(int i) {
+            while (i != id[i]) {
+                id[i] = id[id[i]];
+                i = id[i];
+            }
+            return i;
+        }
     }
 }
