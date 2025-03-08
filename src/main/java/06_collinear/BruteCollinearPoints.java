a8e6ac03a2239ba4f19c3bade51b0ca4ed0928e3
Taras <tsmereka@protonmail.com>
2025-03-08 09:00:58 -0600

    Hacked up an API

---
+++ src/main/java/06_collinear/BruteCollinearPoints.java
@@ -0,0 +1,33 @@
+
+
+/** Write a program BruteCollinearPoints.java that examines 4 points at a time
+ * and checks whether they all lie on the same line segment, returning all such
+ * line segments. To check whether the 4 points p, q, r, and s are collinear,
+ * check whether the three slopes between p and q, between p and r, and between
+ * p and s are all equal.
+ * Performance Requirement: The order of growth of the running time of your
+ * program should be O(n^4)  * in the worst case, and it should use space
+ * proportional to n plus the number of line segments returned.*/
+public class BruteCollinearPoints {
+    /** Finds all line segments containing 4 points. Throw an IllegalArgumentException
+     * if the argument to the constructor is null, if any point in the array is null,
+     * or if the argument to the constructor contains a repeated point. */
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+
+    /** The number of line segments */
+    public int numberOfSegments() {
+
+    }
+
+    /** The line segments. The method segments() should include each line segment
+     * containing 4 points exactly once. If 4 points appear on a line segment in
+     * the order p→q→r→s, then you should include either the line segment p→s or
+     * s→p (but not both) and you should not include subsegments such as p→r or q→r.
+     * For simplicity, we will not supply any input to BruteCollinearPoints
+     * that has 5 or more collinear points. */
+    public LineSegment[] segments() {
+
+    }
+}
---
+++ src/main/java/06_collinear/FastCollinearPoints.java
@@ -0,0 +1,73 @@
+
+/** Remarkably, it is possible to solve the problem much faster than the
+ * brute-force solution described above. Given a point p, the following method4
+ * determines whether p participates in a set of 4 or more collinear points.
+ * 1. Think of p as the origin.
+ * 2. For each other point q, determine the slope it makes with p.
+ * 3. Sort the points according to the slopes they makes with p
+ * 4. Check if any 3 (or more) adjacent points in the sorted order have equal
+ *    slopes with respect to p. If so, these points, together with p, are collinear.
+ * Applying this method for each of the n points in turn yields an efficient
+ * algorithm to the problem. The algorithm solves the problem because points
+ * that have equal slopes with respect to p are collinear, and sorting brings
+ * such points together. The algorithm is fast because the bottleneck operation is sorting.
+ *
+ * Performance requirement. The order of growth of the running time of your program should
+ * be O(n^2 log n) in the worst case and it should use space proportional to n plus the
+ * number of line segments returned. FastCollinearPoints should work properly even if the
+ * input has 5 or more collinear points.
+ * */
+public class FastCollinearPoints {
+    /** Finds all line segments containing 4 or more points. Throw an IllegalArgumentException
+     * if the argument to the constructor is null, if any point in the array is null,
+     * or if the argument to the constructor contains a repeated point. */
+    public FastCollinearPoints(Point[] points) {
+
+    }
+
+    /** The number of line segments */
+    public int numberOfSegments() {
+
+    }
+
+    /** The method segments() should include each maximal line segment containing
+     * 4 (or more) points exactly once. For example, if 5 points appear on a line
+     * segment in the order p→q→r→s→t, then do not include the subsegments p→s or q→t. */
+    public LineSegment[] segments() {
+
+    }
+
+    /** This client program takes the name of an input file as a command-line argument;
+     * read the input file (in the format specified below); prints to standard output
+     * the line segments that your program discovers, one per line; and draws to
+     * standard draw the line segments. */
+    public static void main(String[] args) {
+
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        FastCollinearPoints collinear = new FastCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
+    }
+}
